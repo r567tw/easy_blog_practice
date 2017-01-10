2eb7f24fd1df66daf1127ec349f2c57dc47ae193
r567tw <r567tw@users.noreply.github.com>
2017-01-10 23:11:53 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,58 +1,25 @@
 <?php
-$root='../';
+$root='./';
 session_start();
 if (!isset($_SESSION['BLOG']['USER']))
 {
 	header("location:".$root."login.php");
 }
 
-include($root.'db.php');
+include("db.php");
 
-$sql="select * from posts";
-$posts=$db->query($sql);
-$posts=$posts->fetchAll();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<title>Document</title>
+	<title>已登入</title>
 </head>
 <body>
-<h1>Blog Practice</h1>
-
-<a href="add.php" class="btn btn-primary">修改</a>
-<a href="../index.php" class="btn btn-primary">回首頁</a>
-<table class="table table-responsive">
-<thead>
-<th>*</th>
-<th>標題</th>
-<th>文章內容</th>
-<th>建立時間</th>
-<th>更新時間</th>
-<th>#</th>
-</thead>
-<tbody>
-<?php
-foreach ($posts as $key => $value):
-?>
-<tr>
-<td><?=$value['id']?></td>
-<td><?=$value['title']?></td>
-<td><?=$value['post']?></td>
-<td><?=$value['create_time']?></td>
-<td><?=$value['update_time']?></td>
-<td>
-
-<a href="update.php?id=<?=$value['id']?>" class="btn btn-success">修改</a>
-<a href="delete.php?id=<?=$value['id']?>" class="btn btn-danger">刪除</a>
-
-</td>
-</tr>
-<?php endforeach; ?>
-</tbody>
-</table>
-
+	<h1>已登入</h1>
+	<a href="posts">部落格文章列表</a>
+	<a href="logout.php">登出</a>		
 </body>
 </html>
